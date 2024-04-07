45cc65ee556075f577b1a62ecdd9e4a19d828ab4
iffervescence <iffervescence@gmail.com>
2024-04-07 16:45:30 +0100

    introduce svg

---
+++ index.js
@@ -0,0 +1,9 @@
+/* Shapes */
+var svgContainer = document.getElementById('svgContainer');
+var animItem = bodymovin.loadAnimation({
+  wrapper: svgContainer,
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'flow1.json'
+});
